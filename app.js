847bd18ab13adc94f91dd25395847889ca1c05aa
Prathmesh Channujwar <prathamchannujwar2002@gmail.com>
2025-02-13 23:51:33 +0530

    Styling of listing and new page is done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
 
+
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
@@ -23,6 +24,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
 
 app.get ("/", (req, res) => {
     res.send("Hi, I'm Root");
